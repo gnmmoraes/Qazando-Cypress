edc1c70eb0c641346673cdc89d1757e81833d020
Gabriel Moraes <gnm_moraes@hotmail.com>
2023-09-26 14:55:06 -0300

    Feat: Alteração-Login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,9 +9,34 @@ const userDataInvalid  = require ('../fixtures/cadastro_user_data_invalid.json')
 
 
 describe('Login', () => {
-  
-    it ('Acessar tela', () => {
-       
+
+    beforeEach('Acessando pagina login', () => {
+        commum_page.acessandoLoginUsuario()
+    })
+
+    it('Login email em branco', () => {
+        login_page.preencherLogin('', userDataValid.password)
+        validation_page.validMessageLogin('E-mail inválido.')
+    })
+
+    it('Login email incorreto', () => {
+        login_page.preencherLogin(userDataInvalid.email, userDataValid.password)
+        validation_page.validMessageLogin('E-mail inválido.')
+    })
+
+    it('Login senha em branco', () => {
+        login_page.preencherLogin(userDataValid.email, '')
+        validation_page.validMessageLogin('Senha inválida.')
+    })
+
+    it('Login senha incorreta', () => {
+        login_page.preencherLogin(userDataValid.email, userDataInvalid.password)
+        validation_page.validMessageLogin('Senha inválida.')
+    })
+
+    it('Login com sucesso', () => {
+        login_page.preencherLogin(userDataValid.email, userDataValid.password)
+        validation_page.validCadastroSucesso('Login realizado')
     })
 
 })
